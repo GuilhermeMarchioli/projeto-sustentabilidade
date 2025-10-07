af3863e3aab696c365bd240ea6d17bdafe9a6ab5
Guilherme da Silva Marchioli <marchioliprogrammer@gmail.com>
2025-10-07 16:47:56 -0300

    proseguindo com o desenvolvimento da section problemas
    adicionadas as features: textos, carrocel de imagens e progressiojn bar

--- script.js
+++ script.js
@@ -26,6 +26,70 @@ tsParticles.load("tsparticles", {
       }
     });
 
+  //  Initialize Swiper 
+    var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });    
+
+
+// Porrcentagem animation
+var bar = new ProgressBar.Line('#sp-bar', {
+  strokeWidth: 4,
+  color: '#388e71ff',
+  trailColor: '#ebebebff',
+  trailWidth: 4,
+  duration: 1400,
+  easing: 'easeInOut'
+});
+bar.animate(0.11); 
+
+var bar = new ProgressBar.Line('#sd-bar', {
+  strokeWidth: 4,
+  color: '#388e71ff',
+  trailColor: '#ebebebff',
+  trailWidth: 4,
+  duration: 1400,
+  easing: 'easeInOut'
+});
+bar.animate(0.49); 
+
+var bar = new ProgressBar.Line('#ce-bar', {
+  strokeWidth: 4,
+  color: '#388e71ff',
+  trailColor: '#ebebebff',
+  trailWidth: 4,
+  duration: 1400,
+  easing: 'easeInOut'
+});
+bar.animate(0.08); 
+
+var bar = new ProgressBar.Line('#no-bar', {
+  strokeWidth: 4,
+  color: '#388e71ff',
+  trailColor: '#ebebebff',
+  trailWidth: 4,
+  duration: 1400,
+  easing: 'easeInOut'
+});
+bar.animate(0.07);
+
+var bar = new ProgressBar.Line('#nr-bar', {
+  strokeWidth: 4,
+  color: '#388e71ff',
+  trailColor: '#ebebebff',
+  trailWidth: 4,
+  duration: 1400,
+  easing: 'easeInOut'
+});
+bar.animate(0.24); 
+
 // MenuHumburguer
 function mudouTamanho() {
     if (window.innerWidth >= 768) {
